81bc5bcc429e798f6e4d9292eabb9f527f73bf4e
Hugo Sánchez <hugosanchezmartinez4@gmail.com>
2024-11-08 02:22:13 +0100

    Cambios en el controlador

--- src/main/java/umu/tds/controlador/AppChat.java
+++ src/main/java/umu/tds/controlador/AppChat.java
@@ -6,36 +6,51 @@ import umu.tds.model.Mensaje;
 import umu.tds.model.RepositorioUsuarios;
 import umu.tds.model.Usuario;
 
+/**
+ * Clase controlador de la aplicación.
+ */
 public class AppChat {
-	
-	// Atributos
+	/* TODO: IMPLEMENTAR Singleton */
+	private static AppChat singleton = null;
+
+	/* Atributos */
 	private Usuario user;
 	private RepositorioUsuarios repoUsuarios;
 	
-	// Consulta
-	public Usuario getCurrentUser() {
-		return user;
-	}
-
-	public void setUser(Usuario user) {
-		this.user = user;
-	}
-
-	// CAMBIAR: habrá que pasar uuna copia como en POO
-	public RepositorioUsuarios getRepoUsuarios() {
-		return repoUsuarios;
-	}
-
-	public void setRepoUsuarios(RepositorioUsuarios repoUsuarios) {
+	/* Constructor */
+	private AppChat(Usuario user, RepositorioUsuarios repoUsuarios) {
+		this.user = null;
 		this.repoUsuarios = repoUsuarios;
 	}
 	
-	// Métodos
+	/* Consulta */
+	public Usuario getCurrentUser() {
+		return user;
+	}
+	
+	/* Métodos */
+    /**
+     * Inicia sesión en la aplicación.
+     * 
+     * @param username el nombre de usuario.
+     * @param password la contraseña del usuario.
+     * @return 	{@code false} si no existe un usuario registrado con ese nombre 
+     * 			o la contraseña es incorrecta, {@code true} si se ha iniciado
+     * 			sesión exitosamente.
+     */
+	public boolean login(String username, String password) {
+		Usuario usuarioReg = repoUsuarios.getUserByUsername(username);
+		if (!isUserRegistered(username) || !usuarioReg.getPassword().equals(password)) return false;
+		
+		this.user = usuarioReg;
+		
+		return true;
+	}
+	
 	public boolean isUserRegistered(String username) {
 		return repoUsuarios.getUserByUsername(username) != null;
 	}
 	
 	public static List<Mensaje> obtenerMensajesRecientesPorUsuario;
-	
 	
 }
